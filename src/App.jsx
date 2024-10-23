f05082e737b9713bd016d77f4fe43f2c48a87568
Siddhanth Kheria <sidkheria@Siddhanths-MacBook-Pro.local>
2024-10-23 14:16:24 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
